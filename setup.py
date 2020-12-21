118c76f178db1726f519889a5499bf657c41f333
Ricardo Santos <rsantos@DESKTOP-IL0JHIM.localdomain>
2020-12-21 01:48:24 +0100

    fix(project): change project name on setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='buho-helpers-mixins',
+    name='django-mixins',
     version='0.1.0',
     packages=find_namespace_packages(include=['buho.*']),
     include_package_data=True,
